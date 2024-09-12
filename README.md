947c978fa245368cfe377adc62974c89bc50f3d7
Nishad Tardalkar <nishadtardalkar97@gmail.com>
2024-09-12 00:11:44 +0000

    Fixed the code. Saved modified traces.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # hw2
 SE HW 2
+
+![Static Badge](https://img.shields.io/badge/language-python-blue)
+![Static Badge](https://img.shields.io/github/license/SRN-SE-Fall24/hw1.svg)
+![Static Badge](https://img.shields.io/badge/platform-linux-red)
+![Static Badge](https://github.com/SRN-SE-Fall24/hw2/actions/workflows/python-app.yml/badge.svg)
+![Static Badge](https://github.com/SRN-SE-Fall24/hw2/blob/main/coverage/cov.svg)
--- rand.py
+++ rand.py
@@ -1,10 +1,12 @@
+''' Random array generator. '''
 import subprocess
 
 
 def random_array(arr):
+    ''' Accepts a blank array and fills it with random values. '''
     shuffled_num = None
-    for i in range(len(arr)):
+    for _, i in enumerate(arr):
         shuffled_num = subprocess.run(
-            ["shuf", "-i1-20", "-n1"], capture_output=True)
+            ["shuf", "-i1-20", "-n1"], capture_output=True, check=False)
         arr[i] = int(shuffled_num.stdout)
     return arr

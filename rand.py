99e13a331dc681d8a703e898fa43b1639493fd91
Sharmeen Momin <129156227+SharmeenMomin@users.noreply.github.com>
2024-09-12 00:30:44 +0000

    Fixed logic in the code

--- rand.py
+++ rand.py
@@ -5,7 +5,7 @@ import subprocess
 def random_array(arr):
     ''' Accepts a blank array and fills it with random values. '''
     shuffled_num = None
-    for _, i in enumerate(arr):
+    for i, _ in enumerate(arr):
         shuffled_num = subprocess.run(
             ["shuf", "-i1-20", "-n1"], capture_output=True, check=False)
         arr[i] = int(shuffled_num.stdout)

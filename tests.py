5f9b41bef9f5d7139c44247f791ca114cb34e1b6
Sharmeen Momin <129156227+SharmeenMomin@users.noreply.github.com>
2024-09-13 03:45:01 +0000

    renamed test

--- tests.py
+++ tests.py
@@ -1,22 +1,8 @@
 import hw2_debugging
 
 
-def test_sorted_array():
+def test_with_sorted_array():
     arr = [10, 11, 12, 4, 7, 1, 3]
-    sorted_arr = hw2_debugging.merge_sort(arr)
-
-    assert sorted(arr) == sorted_arr
-
-
-def test_reverse_sorted_array():
-    arr = [10, 8, 6, 5, 3, 2, 1]
-    sorted_arr = hw2_debugging.merge_sort(arr)
-
-    assert sorted(arr) == sorted_arr
-
-
-def test_array_with_duplicates():
-    arr = [10, 8, 6, 2, 5, 3, 3, 10, 2, 1]
     sorted_arr = hw2_debugging.merge_sort(arr)
 
     assert sorted(arr) == sorted_arr

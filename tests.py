5eeb21cf20f5eeb6c0060cf417f00915eaeb7d65
Sharmeen Momin <129156227+SharmeenMomin@users.noreply.github.com>
2024-09-13 03:50:59 +0000

    Added test with duplicate elements

--- tests.py
+++ tests.py
@@ -13,3 +13,10 @@ def test_reverse_sorted_array():
     sorted_arr = hw2_debugging.merge_sort(arr)
 
     assert sorted(arr) == sorted_arr
+
+
+def test_array_with_duplicates():
+    arr = [10, 8, 6, 2, 5, 3, 3, 10, 2, 1]
+    sorted_arr = hw2_debugging.merge_sort(arr)
+
+    assert sorted(arr) == sorted_arr

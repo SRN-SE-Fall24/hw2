bd1751312214da2e731ee19977920fcdf74c03c4
Sharmeen Momin <129156227+SharmeenMomin@users.noreply.github.com>
2024-09-13 03:48:57 +0000

    Added test with reverse sorted array

--- tests.py
+++ tests.py
@@ -6,3 +6,10 @@ def test_with_sorted_array():
     sorted_arr = hw2_debugging.merge_sort(arr)
 
     assert sorted(arr) == sorted_arr
+
+
+def test_reverse_sorted_array():
+    arr = [10, 8, 6, 5, 3, 2, 1]
+    sorted_arr = hw2_debugging.merge_sort(arr)
+
+    assert sorted(arr) == sorted_arr

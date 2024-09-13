949519e64d2f950097d136c721ffd5230f32137f
Sharmeen Momin <129156227+SharmeenMomin@users.noreply.github.com>
2024-09-13 03:59:37 +0000

    Fixed test cases

--- tests.py
+++ tests.py
@@ -2,21 +2,21 @@ import hw2_debugging
 
 
 def test_with_sorted_array():
-    arr = [10, 11, 12, 4, 7, 1, 3]
-    sorted_arr = hw2_debugging.merge_sort(arr)
+    arr = [10, 2, 12, 4, 7, 1, 3]
+    merge_sorted_arr = hw2_debugging.merge_sort(arr)
 
-    assert sorted(arr) == sorted_arr
+    assert sorted(arr) == merge_sorted_arr
 
 
 def test_reverse_sorted_array():
     arr = [10, 8, 6, 5, 3, 2, 1]
-    sorted_arr = hw2_debugging.merge_sort(arr)
+    merge_sorted_arr = hw2_debugging.merge_sort(arr)
 
-    assert sorted(arr) == sorted_arr
+    assert sorted(arr) == merge_sorted_arr
 
 
 def test_array_with_duplicates():
     arr = [10, 8, 1, 2, 5, 3, 3, 10, 2, 1]
-    sorted_arr = hw2_debugging.merge_sort(arr)
+    merge_sorted_arr = hw2_debugging.merge_sort(arr)
 
-    assert sorted(arr) == sorted_arr
+    assert sorted(arr) == merge_sorted_arr

e2b86bdf83cc765e530c0ef5fae028e22f9e3c8c
Sharmeen Momin <129156227+SharmeenMomin@users.noreply.github.com>
2024-09-13 03:53:18 +0000

    Ran static tool analysis

--- tests.py
+++ tests.py
@@ -16,7 +16,7 @@ def test_reverse_sorted_array():
 
 
 def test_array_with_duplicates():
-    arr = [10, 8, 6, 2, 5, 3, 3, 10, 2, 1]
+    arr = [10, 8, 1, 2, 5, 3, 3, 10, 2, 1]
     sorted_arr = hw2_debugging.merge_sort(arr)
 
     assert sorted(arr) == sorted_arr
